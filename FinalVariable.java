bb154aff0629263dba264091056ea65f06a739c4
ebaadazam <ebaadazam82881@gmail.com>
2024-02-10 12:11:55 +0530

    delete

--- FinalVariable.java
+++
@@ -1,7 +0,0 @@
-public class FinalVariable {
-    public static void main(String[] args) {
-        final int num = 10; // we have declared a variable as final
-        // num = 99; // now this line will produce an error
-        System.out.println("The value of num is " + num);
-    }
-}
