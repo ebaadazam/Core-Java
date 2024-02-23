80f87a89a00d075f6a29f155e7a2d7e3af351eab
Ebaad Azam <103363195+ebaadazam@users.noreply.github.com>
2024-02-23 10:14:10 +0530

    Update TryCatchBlock.java

--- Exception Handling/TryCatchBlock.java
+++ Exception Handling/TryCatchBlock.java
@@ -1,1 +1,18 @@
-
+public class TryCatchExample4 {  
+  
+    public static void main(String[] args) {  
+      int val = 20;
+        try  
+        {  
+        int data = val / 0; //may throw exception   
+        }  
+        // handling the exception by using Exception class      
+        catch(Exception e)  
+        {  
+            System.out.println("Arithmetic Exception Occurred");
+            System.out.println(e.getMessage());  
+        }  
+        System.out.println("rest of the code");  
+    }  
+      
+}  
