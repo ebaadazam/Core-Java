bc0c78e9883ef32e1d6bc1844c46496d0ca834a4
Ebaad Azam <103363195+ebaadazam@users.noreply.github.com>
2024-02-23 10:19:27 +0530

    Update MultiCatchBlock.java

--- Exception Handling/MultiCatchBlock.java
+++ Exception Handling/MultiCatchBlock.java
@@ -1,1 +1,29 @@
+public class MultipleCatchBlock4 {  
+  
+    public static void main(String[] args) {  
+          
+           try{    
+                String s=null;  // this will raise NullPointer Exception
+                System.out.println(s.length());  
+               }    
 
+             // It's called when ArithmeticException occurs
+               catch(ArithmeticException e)  
+                  {  
+                   System.out.println("Arithmetic Exception occurs");  
+                  }    
+
+             // It's called when ArrayIndexOutOfBoundsException occurs
+               catch(ArrayIndexOutOfBoundsException e)  
+                  {  
+                   System.out.println("ArrayIndexOutOfBounds Exception occurs");  
+                  }    
+
+             // It's called when ant type of exception occurs
+               catch(Exception e)  
+                  {  
+                   System.out.println("Parent Exception occurs");  
+                  }             
+               System.out.println("rest of the code");    
+    }  
+} 
