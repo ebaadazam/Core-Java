2c0e293f05de19bc43e2d4ee868fec07e2cd0a22
Ebaad Azam <103363195+ebaadazam@users.noreply.github.com>
2024-02-23 10:31:53 +0530

    Update ThrowKeyword.java

--- Exception Handling/ThrowKeyword.java
+++ Exception Handling/ThrowKeyword.java
@@ -1,1 +1,15 @@
+public class Main {
+  static void checkAge(int age) { 
+    if (age < 18) {
+      throw new ArithmeticException("Access denied - You must be at least 18 years old."); 
+    } else {
+      System.out.println("Access granted - You are old enough!"); 
+    }
+ } 
+ 
+ public static void main(String[] args) { 
+   checkAge(17); 
+   System.out.println("Restttt"); // this will not get printed only if exception occurs
+ } 
+}
 
