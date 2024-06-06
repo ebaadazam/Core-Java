ace0c8d845eb837ecbafbf6217cadc5509f71e57
Ebaad Azam <103363195+ebaadazam@users.noreply.github.com>
2024-06-06 12:38:48 +0530

    Update MergeTwoSortedLists.java

--- LinkedList/MergeTwoSortedLists.java
+++ LinkedList/MergeTwoSortedLists.java
@@ -11,30 +11,39 @@ class LinkedList {
     Node head;
 
     public Node mergeLists(Node head1, Node head2) {
+
+        // Mark temp1 at the head of first list 
         Node temp1 = head1;
+        // Mark temp2 at the head of second list
         Node temp2 = head2;
+
+        // Create a dummy node for linking the list in sorted order starting from the dummy node
         Node dummyNode = new Node(-1);
+        // Place temp at the dummyNode
         Node temp = dummyNode;
 
         while (temp1 != null && temp2 != null) {
+
+            // Move temp1 and temp2 simultaneously and link with the dummy node whichever is smaller
             if (temp1.data <= temp2.data) {
                 temp.next = temp1;
-                temp = temp1;
+                temp = temp1; //points temp at the current node
                 temp1 = temp1.next;
             }
             else {
                 temp.next = temp2;
-                temp = temp2;
+                temp = temp2; //points temp at the current node
                 temp2 = temp2.next;
             }
         }
 
+        // Adding the leftover elements to the list
         if (temp1 != null) 
             temp.next=temp1;
         else 
             temp.next=temp2;
 
-        return dummyNode.next;
+        return dummyNode.next; // dummyNode.next will be the head
     }
 }
 
