b9d2b01cbb2b17a1cfee5632673196a0021af3e9
Ebaad Azam <103363195+ebaadazam@users.noreply.github.com>
2024-06-06 12:33:40 +0530

    Update MergeTwoSortedLists.java

--- LinkedList/MergeTwoSortedLists.java
+++ LinkedList/MergeTwoSortedLists.java
@@ -1,1 +1,70 @@
+class Node {
+    int data;
+    Node next;
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
 
+class LinkedList {
+    Node head;
+
+    public Node mergeLists(Node head1, Node head2) {
+        Node temp1 = head1;
+        Node temp2 = head2;
+        Node dummyNode = new Node(-1);
+        Node temp = dummyNode;
+
+        while (temp1 != null && temp2 != null) {
+            if (temp1.data <= temp2.data) {
+                temp.next = temp1;
+                temp = temp1;
+                temp1 = temp1.next;
+            }
+            else {
+                temp.next = temp2;
+                temp = temp2;
+                temp2 = temp2.next;
+            }
+        }
+
+        if (temp1 != null) 
+            temp.next=temp1;
+        else 
+            temp.next=temp2;
+
+        return dummyNode.next;
+    }
+}
+
+class LinkedList_MergeTwoSortedLists {
+    public static void main(String[] args) {
+    
+    // Creating first sorted linkedlist
+    Node list1 = new Node(2);
+    list1.next = new Node(4);
+    list1.next.next = new Node(8);
+    list1.next.next.next = new Node(10);
+
+    // Creating second sorted linkedlist
+    Node list2 = new Node(1);
+    list2.next = new Node(3);
+    list2.next.next = new Node(3);
+    list2.next.next.next = new Node(6);
+    list2.next.next.next.next = new Node(11);
+    list2.next.next.next.next.next = new Node(14);
+
+    LinkedList obj = new LinkedList();
+
+    Node res = obj.mergeLists(list1, list2);
+    Node result = res;
+    while(result != null){
+        System.out.print(result.data + " -> ");
+        result = result.next;
+        }
+        System.out.println("null");
+
+
+    }
+}
