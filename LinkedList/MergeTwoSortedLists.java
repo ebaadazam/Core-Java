5a8faf5310b062b1cbad00e0b76d2d92676db711
Ebaad Azam <103363195+ebaadazam@users.noreply.github.com>
2024-06-08 18:29:56 +0530

    Update MergeTwoSortedLists.java

--- LinkedList/MergeTwoSortedLists.java
+++ LinkedList/MergeTwoSortedLists.java
@@ -1,3 +1,4 @@
+// Approaach One using the dummy node
 class Node {
     int data;
     Node next;
@@ -73,7 +74,84 @@ class LinkedList_MergeTwoSortedLists {
         result = result.next;
         }
         System.out.println("null");
+    }
+}
 
 
+
+// Approach Two  (Most Probably optimal)
+class Node{
+    int data;
+    Node next;
+    public Node(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
+class LinkedList{
+    Node head;
+    public Node mergeTwoSortedLists(Node head1, Node head2){
+        if (head1 == null){
+            return head2;
+        }
+        else if (head2 == null) {
+            return head1;
+        }
+        
+        Node temp1 = head1;
+        Node temp2 = head2;
+        Node head = null;
+        
+        if (temp2.data <= temp1.data){
+            head = temp2;
+            temp2=temp2.next;
+        }
+        else{
+            head = temp1;
+            temp1=temp1.next;
+        }
+
+        Node current = head;
+        while (temp1 != null && temp2 != null) {
+            if (temp1.data <= temp2.data){
+                current.next = temp1;
+                temp1 = temp1.next;
+            }
+            else{
+                current.next = temp2;
+                temp2 = temp2.next;
+            }
+            current=current.next;
+        }
+        if (temp1 != null) {
+            current.next=temp1;
+        }
+        else{
+            current.next=temp2;
+        }
+        return head;
+    }
+}
+
+class PracticeLinkedList{
+    public static void main(String[] args) {
+        Node list1 = new Node(1);
+        list1.next = new Node(3);
+        list1.next.next = new Node(5);
+        list1.next.next.next = new Node(6);
+
+        Node list2 = new Node(7);
+        list2.next = new Node(8);
+        list2.next.next = new Node(9);
+
+        LinkedList obj = new LinkedList();
+        Node result = obj.mergeTwoSortedLists(list1, list2);
+
+        Node res = result;
+        while(res != null){
+            System.out.print(res.data + " -> ");
+            res = res.next;
+        }
+        System.out.println("null");
     }
 }
